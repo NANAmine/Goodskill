8b98612d7ba127006144c43f739bcc0f9420cabb
NANAmine <1486496165@qq.com>
2018-10-18 14:24:51 +0800

    第一次提交

---
+++ js/istext.js
@@ -0,0 +1,3 @@
+//var user_id=26;
+var city="河北省"+"-廊坊市"+"-广阳区";
+var citynumber=131003;
---
+++ js/urlconfiguration.js
@@ -0,0 +1,16 @@
+//var url_1="http://192.168.0.146:8080/framework/";
+//var url_2="http://192.168.0.146:8080/framework/";
+//var url_3="http://192.168.0.146:8080/framework/";
+//var pic_1="http://192.168.0.146:8080/";
+//var pic_2="http://192.168.0.146:8080/";
+//var pic_3="http://192.168.0.146:8080/";
+//var azurl="http://127.0.0.1:8020";
+//var azurl="http://192.168.0.106:8020"; 
+var url_1="http://47.94.7.71/goodskill/";
+var url_2="http://47.94.7.71/goodskill/";
+var url_3="http://47.94.7.71/goodskill/";
+var pic_1="http://47.94.7.71/";
+var pic_2="http://47.94.7.71/";
+var pic_3="http://47.94.7.71/";
+////var azurl="http://127.0.0.1:8020";
+var azurl="http://47.94.7.71/goodskill/"; 
